9d9e0c0cf84c69400b340ef66dc0303c74184b0c
Galib Hassan <galib.hassan.0@gmail.com>
2021-07-24 08:16:21 +0200

    same as previous commit

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Hello world");
+  res.send("Hello again");
 });
 router.get("/home", (req, res, next) => {
   res.send("home");
